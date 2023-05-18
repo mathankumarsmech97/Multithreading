e101e5909f4b09698fc1b54ab10b953440fbf436
Mathankumar <109581628+mathankumarsmech97@users.noreply.github.com>
2023-05-18 09:54:41 +0530

    Add files via upload

---
+++ Multithreading-8/Multithreading/Program.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+//namespace Multithreading
+//{
+//    class MutlithreadDemo
+//    {
+//        public static void Main()
+//        {
+//            Demo of Threading Pro
+
+//             MultithreadingSample.multithreading();
+
+//            ------------
+
+//            How Thread are Wrok
+
+//            MultithreadingSample2.mutlithreadingDemo1();
+
+//            -------------
+
+//            How Pararmeterized Method are use in and Pararmeterized nonThread
+
+//             MultiThreadingDemo3.test();
+
+//            ---------------
+
+//            MultiThreadJoin
+
+//             MultiThreadingJoinDemo4.mutlithreadingJoinDemo();
+
+//            --------------
+
+//            Thread lock Functionpoiter
+
+//            ThreadLock tr = new ThreadLock();
+//            tr.MultirThreadDemo5();
+
+//            ---------------
+
+//            MultithreadPriority
+
+
+//            MultiThreadPriorityDemo.PriorityDemo();
+
+
+
+
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
